3367d34116af61dc035a831f1b13f8e898e2b8b1
Peter Schueller <schueller.p@gmail.com>
2015-09-29 00:47:30 +0300

    finished first pass over everything

--- example_5_1/example_5_1.py
+++ example_5_1/example_5_1.py
@@ -1,23 +1,23 @@
 import dlvhex
-# Networkx library is used to handle graph tasks.
-import networkx as nx
+import networkx as nx # for graph tasks.
 
-# External atom implementation is similar to regular function  in Java, C etc.
-# friendsOfDegree is name of the function and persons name is input parameter (personOfInterest) 
-# for each personOfInterest external atom will return its direct friends
- 
+# An external atom implementation is similar to a regular function
+# in Java, C, etc. friendsOfDegree is name of the function which
+# has one input parameter (personOfInterest). For each
+# personOfInterest external atom will return its direct friends.
 def friendsOfDegree(personOfInterest):
   # graph of the friends will be loaded from the external file
-  g = nx.read_weighted_edgelist("example_2_1.edgelist", nodetype=str,create_using=nx.DiGraph())
+  g = nx.read_weighted_edgelist("example_2_1.edgelist",
+    nodetype=str,create_using=nx.DiGraph())
 
   # Take successor nodes of the node of interest.
   friendList = g.successors(personOfInterest.value())
   # Output the successor nodes
   for node in friendList:
-    dlvhex.output((node,)) # outputs direct fiends
+    dlvhex.output( (node,) ) # outputs (tuple of size one)
     
 # Register function
 def register():
   prop = dlvhex.ExtSourceProperties()
-  prop.addFiniteOutputDomain ( 0 )
+  prop.addFiniteOutputDomain(0) # specify that the graph is finite
   dlvhex.addAtom("friendsOfDegree", (dlvhex.CONSTANT,), 1, prop)
--- example_5_3/example_5_3.py
+++ example_5_3/example_5_3.py
@@ -1,17 +1,17 @@
 import dlvhex
 
-# concat has one input parameter of type tuple (=arbitrarily many constants),
-# which specifies the terms to be concatenated
+# concat has one input parameter of type tuple (=arbitrarily
+# many constants), which specifies the terms to be concatenated
 def concat(tup):
-	# start with empty string and append all input constants in sequence
-	ret = ""
-	for x in tup:
-		ret = ret + x.value()
+  # start with empty string and sequentialy append all inputs
+  ret = ""
+  for x in tup:
+    ret = ret + x.value()
 
-	# output the final string
-	dlvhex.output((ret, ))
+  # output the final string
+  dlvhex.output((ret, ))
 # register all external atoms
 def register():
-	# concat has arbitrarily many input parameters
-	# of type constant (=TUPLE) and its output arity is 1
-	dlvhex.addAtom("concat", (dlvhex.TUPLE, ), 1)
+  # concat has arbitrarily many input parameters
+  # of type constant (=TUPLE) and its output arity is 1
+  dlvhex.addAtom("concat", (dlvhex.TUPLE, ), 1)
