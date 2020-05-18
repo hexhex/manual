10438e9fc8229e69e49b7318d3ec178bf68df734
Peter Schueller <schueller.p@gmail.com>
2020-05-18 09:10:15 +0200

    fixing bug with logo and no longer removing built PDF - will commit PDF to repo for easier access

--- remove_built_files.sh
+++ remove_built_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git filter-branch --prune-empty --index-filter \
-  'git rm -rf --cached --ignore-unmatch *.pdf *.aux *.synctex.gz *.log *.toc *.bbl *~' \
+  'git rm -rf --cached --ignore-unmatch *.aux *.synctex.gz *.log *.toc *.bbl *~' \
   --tag-name-filter cat -- --all
